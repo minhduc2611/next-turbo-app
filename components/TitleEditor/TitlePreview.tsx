2f0bff06677fe837f2fb934d360f31b8f376eb9d
minhducle <minhduc261298@gmail.com>
2023-03-09 17:41:06 +0700

    Add code

--- components/TitleEditor/TitlePreview.tsx
+++ components/TitleEditor/TitlePreview.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 const TitlePreview = ({ value }: Props) => {
   return (
-    <div className={`p-3 w-full`}>
+    <div className={`w-full`}>
       <div
         className="focus-visible:cursor-auto font-bold align-middle w-full resize-none text-5xl text-white bg-inherit focus-visible:border-none focus-visible:outline-none focus-visible:shadow-none"
       >
