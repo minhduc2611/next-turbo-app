8ac402704e8c86d27da6316ed543b5edb3c5d6f7
minhducle <minhduc261298@gmail.com>
2023-03-09 17:06:13 +0700

    Add files

--- components/TextAreaEditor/TextAreaPreview.tsx
+++ components/TextAreaEditor/TextAreaPreview.tsx
@@ -1,5 +1,6 @@
 "use server";
 import React from "react";
+import parse from "html-react-parser";
 
 interface Props {
   value?: string;
@@ -8,14 +9,9 @@ interface Props {
 const TextAreaPreview: React.FC<Props> = ({ value = "" }) => {
   return (
     <div className="font-normal text-lg leading-7 mt-1 w-full focus-visible:outline-none resize-none rounded-[8px] py-3 focus-visible:shadow-none text-light-black">
-      <p
-        dangerouslySetInnerHTML={{
-          __html: `
-      <p style="white-space: pre-line">
+      {parse(`<p style="white-space: pre-line">
       ${value}
-      </p>`,
-        }}
-      ></p>
+      </p>`)}
     </div>
   );
 };
