cb4d174ed2d170133cf7553f2d949fff43e3779c
minhducle <minhduc261298@gmail.com>
2023-03-09 17:18:44 +0700

    add files

--- components/TextAreaEditor/TextAreaPreview.tsx
+++ components/TextAreaEditor/TextAreaPreview.tsx
@@ -1,17 +1,35 @@
 "use server";
 import React from "react";
-import parse from "html-react-parser";
+import parse, {
+  DOMNode,
+  Element,
+  HTMLReactParserOptions,
+} from "html-react-parser";
 
 interface Props {
   value?: string;
 }
 
 const TextAreaPreview: React.FC<Props> = ({ value = "" }) => {
+  const options: HTMLReactParserOptions = {
+    replace: (domNode: DOMNode) => {
+      if (
+        domNode instanceof Element &&
+        domNode?.attribs &&
+        domNode?.attribs.class === "remove"
+      ) {
+        return <></>;
+      }
+    },
+  };
   return (
     <div className="font-normal text-lg leading-7 mt-1 w-full focus-visible:outline-none resize-none rounded-[8px] py-3 focus-visible:shadow-none text-light-black">
-      {parse(`<span style="white-space: pre-line">
+      {parse(
+        `<span style="white-space: pre-line">
       ${value}
-      </span>`)}
+      </span>`,
+        options
+      )}
     </div>
   );
 };
